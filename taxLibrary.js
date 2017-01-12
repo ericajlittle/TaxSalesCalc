a51b64e93134b3e5a2c42ec4a1831249ba934393
ericajlittle <ericalittle123@gmail.com>
2017-01-12 05:54:19 +0000

    finished tax and sales calculator for week1day3 task

--- taxLibrary.js
+++ taxLibrary.js
@@ -21,36 +21,68 @@ var companySalesData = [
     sales: [ 500, 100 ]
   }
 ];
-var eachCompany = {
-    name: "Telus",
-    province: "BC",
-    sales: [ 100, 200, 400 ]
-  };
 
+
+// function taxBranch(companySalesData) {
+//   for (var i =0; i < eachCompany.sales.length; i++) {
+//     var currentCompany = companySalesData[i];
+//     var branchTaxRate = salesTaxRates[currentCompany.province];
+//     var branchTaxTotal = addCompanySales(currentCompany) * branchTaxRate;
+
+//     console.log(branchTaxTotal);
+// }
+
+// function eachCompSales(companySalesData) {
+//   for (var i = 0; i < companySalesData.length; i++) {
+//     var currentCompany = companySalesData[i];
+//     var currentSalesTotal = addCompanySales(currentCompany);
+//     console.log(currentSalesTotal);
+//   }
+// }
+// eachCompSales(companySalesData);
 
 
 function addCompanySales(eachCompany) {
   var total = 0;
   for (var i =0; i < eachCompany.sales.length; i++) {
     total += eachCompany.sales[i];
-}
+  }
   return total;
 }
-console.log(addCompanySales(eachCompany));
+// console.log(addCompanySales(eachCompany));
 
-// function findCompany(companySalesData) {
-  // for (var i = 0; i < companySalesData.length; i++)
-// }
-
-
-
+function calcBranchTaxTotal(eachCompany, branchTotalSales, taxRates) {
+  var provinceCode = eachCompany.province;
+  var taxRate = taxRates[provinceCode];
+  return taxRate * branchTotalSales;
+}
 
 function calculateSalesTax(salesData, taxRates) {
+  var answer = {};
 
-  // Implement your code here
+  for (var i = 0; i < companySalesData.length; i++) {
+    var currentCompany = companySalesData[i];
+    var currentSalesTotal = addCompanySales(currentCompany);
+    var currentTaxTotal = calcBranchTaxTotal(currentCompany, currentSalesTotal, taxRates);
+    var currentCompanyName = currentCompany.name;
+
+    if (answer[currentCompanyName] === undefined) {
+      var partialAnswer = {
+        totalSales: currentSalesTotal,
+        totalTaxes: currentTaxTotal
+      };
+      answer[currentCompanyName] = partialAnswer;
+    } else {
+      answer[currentCompanyName].totalSales += currentSalesTotal;
+      answer[currentCompanyName].totalTaxes += currentTaxTotal;
+    }
+  }
+
+  return answer;
 }
 
 var results = calculateSalesTax(companySalesData, salesTaxRates);
+console.log(results);
 
 /* Expected Results:
 {
