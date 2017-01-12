a744940741ffb2908262c60723968fb3e2f0e7c6
ericajlittle <ericalittle123@gmail.com>
2017-01-12 03:33:38 +0000

    created a function to add sales

---
+++ taxLibrary.js
@@ -0,0 +1,66 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+var eachCompany = {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  };
+
+
+
+function addCompanySales(eachCompany) {
+  var total = 0;
+  for (var i =0; i < eachCompany.sales.length; i++) {
+    total += eachCompany.sales[i];
+}
+  return total;
+}
+console.log(addCompanySales(eachCompany));
+
+// function findCompany(companySalesData) {
+  // for (var i = 0; i < companySalesData.length; i++)
+// }
+
+
+
+
+function calculateSalesTax(salesData, taxRates) {
+
+  // Implement your code here
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
